ad05dfae18ae80f66d4eb641e82d5d802d1ace97
commander-spaceman <206952716+commander-spaceman@users.noreply.github.com>
2026-06-17 15:50:03 -0500

    refactor: minimal layout with Tailwind + daisyUI

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "cmdr.blog";
+export const SITE_DESCRIPTION =
+  "A personal blog about code, projects, and the galaxy.";
