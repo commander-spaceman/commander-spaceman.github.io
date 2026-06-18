0dfd6bf4aa216454aa56a7c1c37046fd562ac60c
commander-spaceman <206952716+commander-spaceman@users.noreply.github.com>
2026-06-18 08:30:27 -0500

    feat: add quarian-voice-filter to featured projects

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@ export const SITE_TITLE = "cmdr.blog";
 export const SITE_DESCRIPTION =
   "A personal blog about code, projects, and the galaxy.";
 export const GITHUB_USER = "commander-spaceman";
-export const FEATURED_REPOS = ["pcc-toolkit", "nara"];
+export const FEATURED_REPOS = ["pcc-toolkit", "nara", "quarian-voice-filter"];
